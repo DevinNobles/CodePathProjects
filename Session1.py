656c9a8f1bc9c77455a5e0ab61e7541758135cad
DevinNobles <144385943+DevinNobles@users.noreply.github.com>
2024-09-19 10:32:22 -0400

    Create Session1.py

---
+++ Session1.py
@@ -0,0 +1,62 @@
+def hello_world():
+    print("Hello World")
+
+hello_world()
+
+
+def todays_mood():
+    mood = "😎"
+    print("Today's mood: " + mood)
+
+todays_mood()
+
+
+# Question 3
+
+def print_menu(menu):
+    print("Lunch today is: " + menu)
+
+print_menu('cheese')
+
+#Question 4
+a = 13
+b = 27
+
+add = (a + b)
+
+def sum(a, b):
+    return a+b
+
+
+def double():
+    dub = add  *2
+    print(dub)
+
+sum(a,b)
+print(add)
+
+double()
+
+
+#Question 5
+def product(a,b):
+    prod = a * b
+    print(prod)
+
+
+product(22,7)
+
+animalName = input()
+print("name is " + animalName)
+
+
+#Questin 6
+def classify_age():
+    age = input()
+
+    if(age < 18):
+        print("Child")
+    else:
+        print("Adult")
+
+classify_age()
