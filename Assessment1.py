eb5a73b9f6e32ec97d9d2c6982d214de82f26df0
DevinNobles <144385943+DevinNobles@users.noreply.github.com>
2024-09-19 14:44:59 -0400

    Update Assessment1.py

--- Assessment1.py
+++ Assessment1.py
@@ -1,3 +1,9 @@
+import math
+import os
+import random
+import re
+import sys
+
 #Write a function that returns the sum of all the Elements in a list. 
 
 # Complete the 'find_sum' function below.
@@ -12,3 +18,47 @@ def find_sum(lst):
     for num in lst:
         sum += num
     return sum 
+
+
+
+#!/bin/python3
+
+
+
+
+#Write a function that finds the minimum value in a list of integers.
+
+# Complete the 'find_min' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY lst as parameter.
+#
+
+def find_min(lst):
+    # Write your code here
+    min = lst[0]
+    for num in lst:
+        if num < min:
+            min = num
+    return min
+
+"""
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    temp = input()
+    
+    if len(temp) > 60:
+        input_string_new = temp
+        chunks_new = input_string_new.split(", ")
+        list_of_lists_new = [list(map(int, chunk.split())) for chunk in chunks_new]
+        result = [find_min(lst) for lst in list_of_lists_new]
+    else:
+        result = find_min([int(n) for n in temp.split()])
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+"""          
